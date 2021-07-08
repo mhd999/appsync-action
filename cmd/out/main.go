5ef7080f75bab8cb0ec9a1b04df8f08e54ca4a9d
Mohamad al moualem <m.almouallem@gmail.com>
2021-07-08 12:50:52 +0200

    debug :( more 1

--- cmd/out/main.go
+++ cmd/out/main.go
@@ -38,7 +38,7 @@ func main() {
 
 		var gihubFiles []string
 
-		root := "/github"
+		root := "/github/workspace"
 		errg := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 			gihubFiles = append(gihubFiles, path)
 			return nil
